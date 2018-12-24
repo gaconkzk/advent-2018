d3b88b5e8eab00928fa3b01826d183287f1a9709
gaconkzk <gaconkzk@gmail.com>
2018-12-24 15:17:15 +0700

    d3 p2 done

--- src/bin/d03.rs
+++ src/bin/d03.rs
@@ -8,11 +8,13 @@ fn main() -> Result<()> {
   let input = fs::read_to_string("input/d03.txt")?;
   p1(&input)?;
 
+  p2(&input)?;
+
   Ok(())
 }
 
-#[derive(Debug)]
 struct Claims {
+  id: u16,
   top: u16,
   left: u16,
   width: u16,
@@ -22,6 +24,7 @@ struct Claims {
 impl Claims {
   fn new(line: &str) -> Claims {
     let parts:Vec<&str> = line.split(' ').collect();
+    let id:u16 = parts[0][1..].parse().unwrap();
     let pos_parts:Vec<&str> = parts[2]
         .split(':')
         .collect();
@@ -38,6 +41,7 @@ impl Claims {
         .collect();
 
     Claims {
+      id,
       left: position[0],
       top: position[1],
       width: size[0],
@@ -46,7 +50,10 @@ impl Claims {
   }
 }
 
-fn fill_fab(claim: &Claims, fab: &mut [[u16; 2000]; 2000]) {
+const F_SIZE:usize = 1000;
+type Fabric = [[usize;F_SIZE]; F_SIZE];
+
+fn fill_fab(claim: &Claims, fab: &mut Fabric) {
   for x in claim.left..claim.left+claim.width {
     for y in claim.top..claim.top+claim.height {
       fab[x as usize][y as usize] += 1;
@@ -54,20 +61,49 @@ fn fill_fab(claim: &Claims, fab: &mut [[u16; 2000]; 2000]) {
   }
 }
 
+fn cool_claim(c: &Claims, fab: &Fabric) -> Option<u16> {
+  for x in c.left..c.left+c.width {
+    for y in c.top..c.top + c.height {
+      if fab[x as usize][y as usize] > 1 {
+        // must use return statement
+        return None
+      }
+    }
+  }
+  // must use return statement
+  return Some(c.id)
+}
+
 fn p1(input: &str) -> Result<()> {
-  let mut fab= [[0; 2000]; 2000];
+  let mut fab= [[0; F_SIZE]; F_SIZE];
 
   input.lines()
       .map(|l| Claims::new(l) )
       .for_each(|c| fill_fab(&c, &mut fab));
 
-  let a:Vec<u16> = fab.iter()
+  let a = fab.iter()
       .flat_map(|arr| arr.iter())
       .map(|a| *a)
       .filter(|f| *f > 1)
+      .collect::<Vec<usize>>()
+      .len();
+
+  println!("p1: {:?}", a);
+
+  Ok(())
+}
+
+fn p2(input: &str) -> Result<()> {
+  let mut fab= [[0; F_SIZE]; F_SIZE];
+
+  let claims:Vec<Claims> = input.lines()
+      .map(|l| Claims::new(l) )
       .collect();
 
-  println!("p1: {:?}", a.len());
+  claims.iter().for_each(|c| fill_fab(&c, &mut fab));
+  let id = claims.iter().find_map(|c| cool_claim(c, &fab)).unwrap();
+
+  println!("p2: {:?}", id);
 
   Ok(())
 }
